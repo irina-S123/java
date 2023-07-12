be81d9829e4388829b13ebd2f13edfc7febdf9fe
irina-S123 <123304775+irina-S123@users.noreply.github.com>
2023-07-12 21:11:13 +0300

    Add files via upload

---
+++ OOP/семинар1/FamilyTree.java
@@ -0,0 +1,23 @@
+package homeWork.family_tree;
+
+import java.util.List;
+
+public class FamilyTree{
+    int id;
+    int humanId;
+    List<Human> humanList;
+
+    public void addHuman(Human human){
+        human.setId(humanId++);
+        humanList.add(human);
+    }
+    public String getHumanInfo(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Список людей:\n");
+        for(Human human: humanList){
+            stringBuilder.append(human);
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
